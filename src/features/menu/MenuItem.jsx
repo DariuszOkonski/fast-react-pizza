611ace65448fa1068c310500ef166ad5342e0224
Dariusz Okoński <darek200180@gmail.com>
2025-06-03 15:36:20 +0200

    add padding between pizzas elements

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -7,8 +7,8 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
+    <li className='flex gap-4 py-2'>
+      <img src={imageUrl} alt={name} className='h-24' />
       <div>
         <p>{name}</p>
         <p>{ingredients.join(', ')}</p>
