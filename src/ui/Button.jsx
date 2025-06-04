159900bb1fcd216b6ef1be5c64968a90a2ffab8e
Dariusz Okoński <darek200180@gmail.com>
2025-06-04 09:13:40 +0200

    style CreateOrder

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 /* eslint-disable react/prop-types */
 function Button({ children, disabled, to, type }) {
   const base =
-    'bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6';
+    'bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 text-sm';
 
   const styles = {
     primary: base + 'px-4 py-3 md:px-6 md:py-4',
     small: base + ' py-2 md:px-5 md:py-2.5 text-sm',
     secondary:
-      'border-2 border-stone-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 px-4 py-2 md:px-6 md:py-3.5 hover:text-stone-800',
+      'border-2 border-stone-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 px-4 py-2 md:px-6 md:py-3.5 hover:text-stone-800 text-sm',
   };
 
   if (to) {
