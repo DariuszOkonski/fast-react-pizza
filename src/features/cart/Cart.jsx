dec5ab3ddc805c84fb988b0ee564c3bf5e5c73af
Dariusz Okoński <darek200180@gmail.com>
2025-06-09 16:20:07 +0200

    display username in Cart and CreateOrder

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,6 +2,7 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
+import { useSelector } from 'react-redux';
 
 const fakeCart = [
   {
@@ -28,6 +29,8 @@ const fakeCart = [
 ];
 
 function Cart() {
+  const username = useSelector((state) => state.user.username);
+
   const cart = fakeCart;
 
   return (
@@ -39,7 +42,7 @@ function Cart() {
         &larr; Back to menu
       </LinkButton>
 
-      <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-xl font-semibold'>Your cart, {username}</h2>
 
       <ul className='divide-y divide-stone-200 border-b mt-3'>
         {cart.map((item) => (
